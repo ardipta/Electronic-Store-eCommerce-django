7276fcd95ac6717acf370a73e6f4849e89a790f7
ardipto <ardipta82@gmail.com>
2020-07-01 23:41:35 +0600

    Template mastering(index, about, mail)

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 
 
 def index(request):
     return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def mail(request):
+    return render(request, 'pages/mail.html')
